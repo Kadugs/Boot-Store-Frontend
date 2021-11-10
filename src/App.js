b18b00c3f03854009ca666653b269766e77dacc9
Kadugs <cadu20.gomes@gmail.com>
2021-11-10 11:11:15 -0300

    implemented Products Details

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import "./App.css";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Details from "./components/Details/Details";
-// path = /products/:id
 export default function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/products/:id">
+        <Route exact path="/products/:id">
           <Details />
         </Route>
       </Switch>
