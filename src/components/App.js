9dc24605941437817ce8d09bb42bb4dc73f6d88c
Kadugs <cadu20.gomes@gmail.com>
2021-11-10 13:02:57 -0300

    creating cart

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,17 @@
-import GlobalStyle from '../styles/globalStyle.js';
-import ProductsList from './ProductsList/ProductsList.js';
-import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
-
+import GlobalStyle from "../styles/globalStyle.js";
+import ProductsList from "./ProductsList/ProductsList.js";
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
+import Cart from "./Cart/Cart";
 export default function App() {
   return (
     <BrowserRouter>
       <GlobalStyle />
       <Switch>
-        <Route path='/' component={ProductsList} exact />
-        <Redirect to='/' />
+        <Route path="/" component={ProductsList} exact />
+        <Redirect to="/" />
+        <Route exact path="/cart">
+          <Cart />
+        </Route>
       </Switch>
     </BrowserRouter>
   );
