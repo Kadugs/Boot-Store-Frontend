4f7eb162b5230e2dd660061e76ffb9d5a31322b0
Luis Felipe <l_felipelizeu@hotmail.com>
2021-11-10 23:50:22 -0300

    Created an user context

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,27 @@
 import GlobalStyle from "../styles/globalStyle.js";
+import { useState } from 'react';
+import UserContext from '../contexts/UserContext.js';
 import Header from "./Header/Header.js";
 import ProductsList from "./ProductsList/ProductsList.js";
 import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 import Details from "./Details/Details";
 
 export default function App() {
+  const [user, setUser] = useState(JSON.parse(localStorage.getItem("user")));
+
   return (
-    <BrowserRouter>
-      <GlobalStyle />
-      <Switch>
-        <Route path="/" component={ProductsList} exact />
-        <Route exact path="/products/:code">
-          <Header />
-          <Details />
-        </Route>
-        <Redirect to="/" />
-      </Switch>
-    </BrowserRouter>
+    <UserContext.Provider value={{ user, setUser }}>
+      <BrowserRouter>
+        <GlobalStyle />
+        <Switch>
+          <Route path="/" component={ProductsList} exact />
+          <Route exact path="/products/:code">
+            <Header />
+            <Details />
+          </Route>
+          <Redirect to="/" />
+        </Switch>
+      </BrowserRouter>
+    </UserContext.Provider>
   );
 }
